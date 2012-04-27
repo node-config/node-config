0de79f4c569a7b211eb0ffc35bbff771493eba8a
Matias Meno <m@tias.me>
2012-04-27 17:56:49 +0200

    The config now parses local.EXT and local-deployment.EXT files as well.
    Also added tests for it.

--- test/2-config-test.js
+++ test/2-config-test.js
@@ -53,6 +53,14 @@ exports.ConfigTest = vows.describe('Test suite for node-config').addBatch({
       assert.equal(CONFIG.Customers.dbPort, '5999');
     },
 
+    'Loading configurations from the local file is correct': function() {
+      assert.equal(CONFIG.Customers.dbPassword, 'real password');
+    },
+
+    'Loading configurations from the local environment file is correct': function() {
+      assert.equal(CONFIG.Customers.dbPassword2, 'another password');
+    },
+
     'Loading prior runtime.json configurations is correct': function() {
       assert.equal(CONFIG.Customers.dbName, 'override_from_runtime_json');
     }
