a66e392f48e9191901f7d8efd6f2a06355286647
Loren West <email@lorenwest.com>
2014-06-19 17:48:48 -0700

    Updated README

--- tools/contributors.js
+++ tools/contributors.js
@@ -71,7 +71,7 @@ getContributorTable(function(error, htmlTable) {
   // Replace the contributor table
   var fileLines = fs.readFileSync(README_FILE).toString().split('\n');
   for (var i in fileLines) {
-    if (fileLines[i].indexOf('<table id="contributors">') === 0) {
+    if (fileLines[i].indexOf('<table id="contributors"') === 0) {
       fileLines[i] = htmlTable;
       fs.writeFileSync(README_FILE, fileLines.join('\n'));
       console.log('Contributor table replaced.');
