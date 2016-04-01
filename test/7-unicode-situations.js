0778f93296080d8515b893c9f3ec0a101cf3cff3
Mark Stosberg <mark@rideamigos.com>
2016-04-01 19:05:10 -0400

    Refactor: Simplify BOM Unicode tests.
    The config files and test coverage for this were a copy/paste job that
    included some dead code and dead config files which could be safely
    removed without reducing the test coverage for this feature.

--- test/7-unicode-situations.js
+++ test/7-unicode-situations.js
@@ -4,40 +4,21 @@ var vows = require('vows'),
     assert = require('assert'),
         path = require('path');
 
-var CONFIG,
-    override;
+// Change the configuration directory for testing
+process.env.NODE_CONFIG_DIR = __dirname + '/7-config';
+
+// Hardcode $NODE_ENV=test for testing
+delete process.env.NODE_ENV;
+
+// Test for multi-instance applications
+delete process.env.NODE_APP_INSTANCE;
+
+process.env.NODE_CONFIG_STRICT_MODE = false;
+
+var CONFIG = requireUncached('../lib/config');
+
 
 vows.describe('Tests for Unicode situations')
-.addBatch({
-    'Byte Order Marker (BOM)': {
-        topic: function () {
-            // Change the configuration directory for testing
-            process.env.NODE_CONFIG_DIR = __dirname + '/7-config';
-
-            // Hardcode $NODE_ENV=test for testing
-            process.env.NODE_ENV = 'test';
-
-            // Test for multi-instance applications
-            process.env.NODE_APP_INSTANCE = '7';
-
-            // Test $NODE_CONFIG environment and --NODE_CONFIG command line parameter
-            process.env.NODE_CONFIG = '{"EnvOverride":{"parm3":"overridden from $NODE_CONFIG","parm4":100}}';
-            process.argv.push('--NODE_CONFIG={"EnvOverride":{"parm5":"overridden from --NODE_CONFIG","parm6":101}}');
-
-            // Test Environment Variable Substitution
-            override = 'CUSTOM VALUE FROM JSON ENV MAPPING';
-            process.env.CUSTOM_JSON_ENVIRONMENT_VAR = override;
-
-            CONFIG = requireUncached('../lib/config');
-
-            return CONFIG;
-
-        },
-        'Config library is available': function () {
-            assert.isObject(CONFIG);
-        }
-    },
-})
 .addBatch({
     'Parsing of BOM related files': {
         'A standard config file having no BOM should continue to parse without error': function () {
