88864626428cd089e5b3113c63f0c2920d94495e
Simon Scherzinger <scherzinger@entrecode.de>
2016-10-21 17:35:10 +0200

    adds second parameter to defer function stub

--- defer.js
+++ defer.js
@@ -1,7 +1,7 @@
 // Create a deferredConfig prototype so that we can check for it when reviewing the configs later.
 function DeferredConfig () {
 }
-DeferredConfig.prototype.resolve = function (config) {};
+DeferredConfig.prototype.resolve = function (config, original) {};
 
 // Accept a function that we'll use to resolve this value later and return a 'deferred' configuration value to resolve it later.
 function deferConfig (func) {
