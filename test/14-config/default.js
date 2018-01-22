66dc2481bcf93e46682720be36d3c3c6979f7b11
vifird <vifirde@gmail.com>
2018-01-22 22:48:04 +0800

    Add Test code to configrc mode.

---
+++ test/14-config/default.js
@@ -0,0 +1,11 @@
+
+var config = {
+  siteName : 'default site name',
+  number : 14,
+};
+
+config.email = {
+    subject : 'default subject',
+}
+
+module.exports = config;
---
+++ test/14-node-configrc.js
@@ -0,0 +1,45 @@
+
+// Tests for configrc\' file
+
+// Dependencies
+var vows = require('vows'),
+    assert = require('assert'),
+    requireUncached = require('./_utils/requireUncached');
+
+vows.describe("CONFIG_RC: Tests for configrc' file")
+.addBatch({
+    "Tests for .configrc": {
+        topic: function() {
+
+          let pre = process.argv;
+          process.argv.pop(process.argv.length-1);
+          
+          delete process.env.NODE_CONFIG;
+
+          process.env.USE_CONFIG_RC = 'true';
+          process.env.CONFIG_RC_DIR = '../test/14-config/configrc';
+          process.env.CONFIG_RC = 'configrc.json';
+
+
+          requireUncached(`${__dirname}/../lib/config`);
+          let config = require('../lib/config');
+          requireUncached(`${__dirname}/../lib/config`);
+
+          delete process.env.USE_CONFIG_RC;
+          delete process.env.CONFIG_RC_DIR;
+          process.argv = pre;
+
+          return config;
+        },
+        "Tests for loading configrc' file": function (config) {
+
+          assert.strictEqual(config.number, 14);
+        },
+        "Test for config merging of .configrc": function (config) {
+          
+          assert.strictEqual(config.configrc, "gotIt");
+        }
+    }
+})
+.export(module);
+
