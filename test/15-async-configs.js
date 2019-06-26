d7463221328789d87948a0ecfa085b98f0bd3017
imoses <ido@twiggle.com>
2019-06-26 03:35:56 +0300

    Add async configuration support - asyncConfig

---
+++ test/15-async-configs.js
@@ -0,0 +1,62 @@
+var resolveAsyncConfigs = require('../async').resolveAsyncConfigs;
+var requireUncached = require('./_utils/requireUncached');
+
+// Test declaring deferred values.
+
+// Change the configuration directory for testing
+process.env.NODE_CONFIG_DIR = __dirname + '/15-config';
+
+// Hardcode $NODE_ENV=test for testing
+process.env.NODE_ENV='test';
+
+// Test for multi-instance applications
+process.env.NODE_APP_INSTANCE='async';
+
+// Because require'ing config creates and caches a global singleton,
+// We have to invalidate the cache to build new object based on the environment variables above
+var CONFIG = requireUncached(__dirname + '/../lib/config');
+
+// Dependencies
+var vows = require('vows'),
+    assert = require('assert');
+
+vows.describe('Tests for async values - JavaScript').addBatch({
+  'Configuration file Tests': {
+    topic: function() {
+      resolveAsyncConfigs(CONFIG).then(function(config) {
+        this.callback(null, config);
+      }.bind(this)).catch(function(err) {
+        this.callback(err);
+      }.bind(this));
+    },
+
+    'Using asyncConfig() in a config file causes value to be evaluated by resolveAsyncConfigs': function() {
+      // The deferred function was declared in default-defer.js
+      // Then local-defer.js is located which overloads the siteTitle mentioned in the function
+      // Finally the deferred configurations, now referencing the 'local' siteTitle
+      assert.equal(CONFIG.welcomeEmail.subject, 'Welcome to New Instance!');
+    },
+
+    'values which are functions remain untouched unless they are instance of AsyncConfig': function() {
+      // If this had been treated as a deferred config value it would blow-up.
+      assert.equal(CONFIG.welcomeEmail.aFunc(), 'Still just a function.');
+    },
+
+    // This defer function didn't use args, but relied 'this' being bound to the main config object
+    "async functions can simply refer to 'this'" : function () {
+      assert.equal(CONFIG.welcomeEmail.justThis, 'Welcome to this New Instance!');
+    },
+
+    "async promises which return objects should still be treated as a single value." : function () {
+      assert.deepEqual(CONFIG.get('map.centerPoint'), { lat: 3, lon: 4 });
+    },
+
+    "async promise return original value." : function () {
+      assert.equal(CONFIG.original.original, 'an original value');
+    },
+
+    "second async promise return local value." : function () {
+      assert.equal(CONFIG.original.originalPromise, 'not an original value');
+    },
+  }
+}).export(module);
