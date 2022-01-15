4a61359f56f91f61d602d5300ce5ebeb4b88a2b5
Jason <jdmarshall@users.noreply.github.com>
2022-01-15 00:52:41 -0800

    Remove state sharing between tests.
    Test-6 is polluting process.env, resulting in errors in any new
    tests I try to write.

--- test/util.js
+++ test/util.js
@@ -1,13 +1,14 @@
+var requireUncached = require('./_utils/requireUncached');
 
 // Tests for config.util functions
+
 
 // Dependencies
 var vows = require('vows'),
     assert = require('assert'),
     path = require('path'),
     config = require('../lib/config'),
-    initParam = config.util.initParam,
-    loadFileConfigs = config.util.loadFileConfigs;
+    initParam = config.util.initParam;
 
 vows.describe('Tests for config util functions')
 .addBatch({
@@ -39,17 +40,27 @@ vows.describe('Tests for config util functions')
         },
     },
     'Tests for util.loadFileConfigs': {
-        'It can load data from a given directory': function () {
-          var result = loadFileConfigs(path.join(__dirname, '5-config'));
+        topic: function () {
+            // Change the configuration directory for testing
+            process.env.NODE_CONFIG_DIR = __dirname + '/config';
+
+            // Hard-code $NODE_CONFIG_ENV=test for testing
+            delete process.env.NODE_APP_INSTANCE;
+            process.env.NODE_CONFIG_ENV='test';
+
+            return requireUncached(__dirname + '/../lib/config').util;
+        },
+        'It can load data from a given directory': function (util) {
+          var result = util.loadFileConfigs(path.join(__dirname, '5-config'));
           assert.strictEqual(result.number, 5);
         },
-        'It ignores NODE_CONFIG when loading from directory': function () {
+        'It ignores NODE_CONFIG when loading from directory': function (util) {
           var prev = process.env.NODE_CONFIG;
-          process.env.NODE_CONFIG = '{"number":4}'
-          var result = loadFileConfigs(path.join(__dirname, '5-config'));
+          process.env.NODE_CONFIG = '{"number":4}';
+          var result = util.loadFileConfigs(path.join(__dirname, '5-config'));
           assert.strictEqual(result.number, 5);
           process.env.NODE_CONFIG = prev;
-        },
+        }
     },
     'Tests for util.isPromise': {
       'It can identify a new Promise': function () {
