29db37616d7a4277766e9b7d8d43069d0539ab44
kaivean <kaivean@outlook.com>
2022-06-23 19:55:35 +0800

    feat: rebase  https://github.com/eloquence/node-config

--- test/6-strict-mode.js
+++ test/6-strict-mode.js
@@ -10,7 +10,7 @@ vows.describe('Tests for strict mode').addBatch({
     NODE_ENV         : 'BOOM',
     APP_INSTANCE     : 'valid-instance',
     exceptionMessage : "FATAL: NODE_ENV value of 'BOOM' did not match any deployment config file names. "
-                     + "See https://github.com/node-config/node-config/wiki/Strict-Mode",
+                     + "Strict mode is on, aborting. See https://github.com/node-config/node-config/wiki/Strict-Mode",
   }),
 
   // Because NODE_ENV=development = default
@@ -24,53 +24,100 @@ vows.describe('Tests for strict mode').addBatch({
     NODE_ENV         : 'production,cloud',
     APP_INSTANCE     : 'valid-instance',
     exceptionMessage : "FATAL: NODE_ENV value of 'cloud' did not match any deployment config file names. "
-                     + "See https://github.com/node-config/node-config/wiki/Strict-Mode"
+                     + "Strict mode is on, aborting. See https://github.com/node-config/node-config/wiki/Strict-Mode"
   }),
 
   "Specifying an unused NODE_APP_INSTANCE and valid NODE_ENV value throws an exception": _expectException({
     NODE_ENV         : 'valid-deployment',
     APP_INSTANCE     : 'BOOM',
     exceptionMessage : "FATAL: NODE_APP_INSTANCE value of 'BOOM' did not match any instance config file names. "
-                     + "See https://github.com/node-config/node-config/wiki/Strict-Mode",
+                     + "Strict mode is on, aborting. See https://github.com/node-config/node-config/wiki/Strict-Mode",
   }),
 
   "NODE_ENV=default throws exception: reserved word": _expectException({
     NODE_ENV         : 'default',
     APP_INSTANCE     : 'valid-instance',
     exceptionMessage :"FATAL: NODE_ENV value of 'default' is ambiguous. "
-                     +"See https://github.com/node-config/node-config/wiki/Strict-Mode",
+                     +"Strict mode is on, aborting. See https://github.com/node-config/node-config/wiki/Strict-Mode",
   }),
 
   "NODE_ENV=production,default throws exception: reserved word": _expectException({
     NODE_ENV         : 'production,default',
     APP_INSTANCE     : 'valid-instance',
     exceptionMessage :"FATAL: NODE_ENV value of 'default' is ambiguous. "
-                     +"See https://github.com/node-config/node-config/wiki/Strict-Mode",
+                     +"Strict mode is on, aborting. See https://github.com/node-config/node-config/wiki/Strict-Mode",
   }),
 
   "NODE_ENV=local throws exception: reserved word": _expectException({
     NODE_ENV         : 'local',
     APP_INSTANCE     : 'valid-instance',
     exceptionMessage :"FATAL: NODE_ENV value of 'local' is ambiguous. "
-                     +"See https://github.com/node-config/node-config/wiki/Strict-Mode",
+                     +"Strict mode is on, aborting. See https://github.com/node-config/node-config/wiki/Strict-Mode",
   }),
 
   "Specifying reserved word for NODE_CONFIG_ENV throws reserved word exception with appropriate wording": _expectException({
     NODE_CONFIG_ENV  : 'local',
     APP_INSTANCE     : 'valid-instance',
     exceptionMessage :"FATAL: NODE_CONFIG_ENV value of 'local' is ambiguous. "
-                     +"See https://github.com/node-config/node-config/wiki/Strict-Mode",
+                     +"Strict mode is on, aborting. See https://github.com/node-config/node-config/wiki/Strict-Mode",
   }),
 
   "Specifying NODE_CONFIG_ENV=production,cloud with no cloud file throws an exception with appropriate wording": _expectException({
     NODE_CONFIG_ENV  : 'cloud',
     APP_INSTANCE     : 'valid-instance',
     exceptionMessage :"FATAL: NODE_CONFIG_ENV value of 'cloud' did not match any deployment config file names. "
-                     +"See https://github.com/node-config/node-config/wiki/Strict-Mode",
+                     +"Strict mode is on, aborting. See https://github.com/node-config/node-config/wiki/Strict-Mode",
   }),
 })
-.export(module);
+.addBatch({
 
+  "Specifying an unused NODE_ENV value and valid NODE_APP_INSTANCE value shows warnings when strict mode is off": _verifyConsoleOutput({
+    NODE_ENV         : 'BOOM',
+    APP_INSTANCE     : 'valid-instance',
+    warningMessage   : "WARNING: NODE_ENV value of 'BOOM' did not match any deployment config file names.",
+    tryToSuppress    : false,
+    canBeSuppressed  : true,
+  }),
+
+  "Specifying an unused NODE_ENV value and valid NODE_APP_INSTANCE shows no warnings when strict mode is off and warnings are suppressed": _verifyConsoleOutput({
+    NODE_ENV         : 'BOOM',
+    APP_INSTANCE     : 'valid-instance',
+    tryToSuppress    : true,
+    canBeSuppressed  : true
+  }),
+
+  "Specifying an unused NODE_APP_INSTANCE and valid NODE_ENV shows warnings when strict mode is off": _verifyConsoleOutput({
+    NODE_ENV         : 'valid-deployment',
+    APP_INSTANCE     : 'BOOM',
+    warningMessage   : "WARNING: NODE_APP_INSTANCE value of 'BOOM' did not match any instance config file names.",
+    tryToSuppress    : false,
+    canBeSuppressed  : true
+  }),
+
+  "Specifying an unused NODE_APP_INSTANCE and valid NODE_ENV shows no warnings when strict mode is off and warnings are suppressed": _verifyConsoleOutput({
+    NODE_ENV         : 'valid-deployment',
+    APP_INSTANCE     : 'BOOM',
+    tryToSuppress    : true,
+    canBeSuppressed  : true
+  }),
+
+  "NODE_ENV=default shows warnings when strict mode is off: reserved word": _verifyConsoleOutput({
+    NODE_ENV         : 'default',
+    APP_INSTANCE     : 'valid-instance',
+    warningMessage   : "WARNING: NODE_ENV value of 'default' is ambiguous.",
+    tryToSuppress    : false,
+    canBeSuppressed  : false
+  }),
+
+  "NODE_ENV=default warning message cannot be suppressed": _verifyConsoleOutput({
+    NODE_ENV         : 'default',
+    APP_INSTANCE     : 'valid-instance',
+    tryToSuppress    : true,
+    canBeSuppressed  : false
+  })
+
+})
+.export(module);
 
 // helper function to create similar tests
 // Pass in NODE_ENV and APP_INSTANCE and an 'exceptionMessage' that is' expected to be generated by the combo under strict mode.
@@ -119,4 +166,77 @@ function _expectException (opts) {
       }
     }
   };
+}
+
+// Helper function similar to above, but geared towards analyzing console output
+function _verifyConsoleOutput (opts) {
+
+  // Appended to all warnings when strict mode is off
+  var strictModeNotice = 'WARNING: Strict mode is off, continuing. See https://github.com/node-config/node-config/wiki/Strict-Mode'
+  // Appended to warnings which can be suppressed
+  var suppressNotice = 'WARNING: To disable this warning, set SUPPRESS_NO_CONFIG_WARNING in the environment.';
+
+  var tests = {
+    topic: function() {
+
+      process.env.NODE_CONFIG_DIR             = __dirname + '/6-config';
+      process.env.NODE_CONFIG_STRICT_MODE     = 0;
+      process.env.NODE_APP_INSTANCE           = opts.APP_INSTANCE;
+      process.env.NODE_ENV                    = opts.NODE_ENV;
+      if (opts.tryToSuppress) {
+        process.env.SUPPRESS_NO_CONFIG_WARNING = 1;
+      } else {
+        // process.env coerces even undefined to 'undefined', so we must delete
+        delete process.env.SUPPRESS_NO_CONFIG_WARNING;
+      }
+      delete process.env.NODE_CONFIG;
+      delete process.env.NODE_CONFIG_ENV;
+
+      var consoleError,
+        messages = [];
+
+      // We collect messages by temporarily overriding console.error
+      consoleError = console.error;
+      console.error = _collectMessage;
+      requireUncached(__dirname + '/../lib/config');
+      console.error = consoleError;
+
+      function _collectMessage(line) {
+        messages.push(line);
+      }
+
+      return messages;
+
+    }
+  };
+
+  if (!opts.tryToSuppress) {
+
+    tests['Expected warning messages are shown when strict mode is off'] = function(messages) {
+      var expectedMessages = [opts.warningMessage, strictModeNotice]
+      if (opts.canBeSuppressed) {
+        expectedMessages.push(suppressNotice);
+      }
+      assert.deepEqual(messages, expectedMessages);
+    }
+
+
+  } else if (opts.tryToSuppress) {
+
+    tests['Warning messages are suppressed when strict mode is off and SUPPRESS_NO_CONFIG_WARNING is set'] = function(messages) {
+      assert.equal(messages.length, 0);
+    }
+
+  }
+
+  return tests;
+
+}
+
+//
+// Because require'ing config creates and caches a global singleton,
+// We have to invalidate the cache to build new object based on the environment variables above
+function requireUncached(module){
+   delete require.cache[require.resolve(module)];
+   return require(module);
 }
