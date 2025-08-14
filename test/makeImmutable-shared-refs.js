45af5ef664be2e43cd4cb0f12fcad662e656861b
Jason <jdmarshall@users.noreply.github.com>
2025-08-13 22:14:22 -0700

    Finish off the medium sized test files, converting to node:test

--- test/makeImmutable-shared-refs.js
+++ test/makeImmutable-shared-refs.js
@@ -1,78 +1,84 @@
+'use strict';
+
+const { describe, it, beforeEach } = require('node:test');
+const assert = require('assert');
+const util = require('../lib/config.js').util;
+
 /**
  * <p>Unit tests for makeImmutable shared reference cases</p>
  *
  * @module test
  */
 
-const vows = require('vows');
-const assert = require('assert');
-const util = require('../lib/config.js').util;
+describe('Tests for makeImmutable shared reference handling', function() {
+  describe('makeImmutable with arrays containing shared objects', function() {
+    let data;
 
-vows.describe('Tests for makeImmutable shared reference handling')
-  .addBatch({
-    'makeImmutable with arrays containing shared objects': {
-      topic: function() {
-        // Create a object that will get get shared.
-        const sharedNetworkObject = { 
-          _id: "12345",
-          name: "Test Network",
-          capabilities: {
-            primaryNetwork: true,
-          }
-        };
-        
-        // Pattern where same object appears in multiple arrays within the configuration
-        return {
-          // This simulates scheduled tasks with shared network references
-          scheduledTasks: [
-            {
-              name: 'task1',
-              options: {
-                networks: [sharedNetworkObject]  // First reference
-              }
-            },
-            {
-              name: 'task2', 
-              options: {
-                networks: [sharedNetworkObject]  // Same object, second reference!
-              }
+    beforeEach(function () {
+      // Create a object that will get get shared.
+      const sharedNetworkObject = {
+        _id: "12345",
+        name: "Test Network",
+        capabilities: {
+          primaryNetwork: true,
+        }
+      };
+
+      // Pattern where same object appears in multiple arrays within the configuration
+      data = {
+        // This simulates scheduled tasks with shared network references
+        scheduledTasks: [
+          {
+            name: 'task1',
+            options: {
+              networks: [sharedNetworkObject]  // First reference
             }
-          ]
-        };
-      },
+          },
+          {
+            name: 'task2',
+            options: {
+              networks: [sharedNetworkObject]  // Same object, second reference!
+            }
+          }
+        ]
+      };
+    });
 
-      'Should not throw error with shared objects in arrays': function(topic) {
-        assert.doesNotThrow(function() {
-          util.makeImmutable(topic);
-        }, /Cannot redefine property/);
-      },
+    it('Should not throw error with shared objects in arrays', function () {
+      assert.doesNotThrow(function () {
+        util.makeImmutable(data);
+      }, /Cannot redefine property/);
+    });
 
-      'Shared objects in arrays should be immutable': function(topic) {
-        const firstTask = topic.scheduledTasks[0];
-        assert.throws(function() {
-          firstTask.options.networks[0].capabilities.primaryNetwork = false;
-        }, /Can not update runtime configuration property/);
-      }
-    },
+    it('Shared objects in arrays should be immutable', function () {
+      util.makeImmutable(data);
 
-    'makeImmutable idempotency': {
-      topic: function() {
-        return { 
-          config: { name: "test" },
-          capabilities: { setting: true } 
-        };
-      },
+      const firstTask = data.scheduledTasks[0];
+      assert.throws(function () {
+        firstTask.options.networks[0].capabilities.primaryNetwork = false;
+      }, /Can not update runtime configuration property/);
+    });
+  });
 
-      'Should not throw error when called twice on same object': function(topic) {
-        // First call
-        util.makeImmutable(topic);
-        
-        // Second call should not throw "Cannot redefine property"
-        assert.doesNotThrow(function() {
-          util.makeImmutable(topic);
-        }, /Cannot redefine property/);
-      }
-    }
-  })
-  .export(module);
+  describe('makeImmutable idempotency', function() {
+    let data;
+
+    beforeEach(function() {
+      data = {
+        config: { name: "test" },
+        capabilities: { setting: true }
+      };
+    });
+
+    it('Should not throw error when called twice on same object', function() {
+      // First call
+      util.makeImmutable(data);
+
+      // Second call should not throw "Cannot redefine property"
+      assert.doesNotThrow(function() {
+        util.makeImmutable(data);
+      }, /Cannot redefine property/);
+    });
+  });
+});
 
