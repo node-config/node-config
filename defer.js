5cd516c79dfd18b34b12a9cb9ce6facbd4c67258
Jason Marshall <jdmarshall@users.noreply.github.com>
2026-01-14 20:53:15 -0800

    Deprecate defer.js

---
+++ defer.js
@@ -0,0 +1,12 @@
+const { deferConfig, DeferredConfig } = require('./lib/defer');
+
+/**
+ * @deprecated please use the new callback mechanism
+ * @see lib/defer.js
+ */
+module.exports.deferConfig = deferConfig;
+
+/**
+ * @deprecated please use the new callback mechanism
+ */
+module.exports.DeferredConfig = DeferredConfig;
